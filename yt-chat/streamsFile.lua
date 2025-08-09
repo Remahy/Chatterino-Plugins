6d1f302caf0b4152b23aa1f651ed2b3b673dbba7
Karar Al-Remahy <alremahykarar@ymail.com>
2025-08-10 01:53:39 +0200

    move latter part from StreamFile_Read to streamFile_create to make code more readable

--- yt-chat/streamsFile.lua
+++ yt-chat/streamsFile.lua
@@ -10,6 +10,19 @@ local STREAMS_FILE_DEFAULT_CONTENT = [[{
   "]] .. STREAMS_CHANNELS_PROPERTY_NAME .. [[": {}
 }]]
 
+local streamFile_create = function()
+  local f, e = io.open(STREAMS_FILE_NAME, "w+")
+  assert(f, e)
+
+  f:seek("set", 0)
+
+  f:write(STREAMS_FILE_DEFAULT_CONTENT):flush()
+  f:close()
+
+  ---@type table
+  return json.decode(STREAMS_FILE_DEFAULT_CONTENT)
+end
+
 function StreamFile_Read()
   if FileExists(STREAMS_FILE_NAME) then
     local f, e = io.open(STREAMS_FILE_NAME, "r+")
@@ -25,16 +38,7 @@ function StreamFile_Read()
     return json.decode(rawFile)
   end
 
-  local f, e = io.open(STREAMS_FILE_NAME, "w+")
-  assert(f, e)
-
-  f:seek("set", 0)
-
-  f:write(STREAMS_FILE_DEFAULT_CONTENT):flush()
-  f:close()
-
-  ---@type table
-  return json.decode(STREAMS_FILE_DEFAULT_CONTENT)
+  return streamFile_create()
 end
 
 ---@param data table
