1172325e1fed351421f42dfe360d6f58e08b641b
Karar Al-Remahy <alremahykarar@ymail.com>
2026-07-28 04:14:10 +0200

    fix reading streams using channel id

--- yt-chat/constants.lua
+++ yt-chat/constants.lua
@@ -3,6 +3,7 @@ YT_CHAT_SYSTEM_MESSAGE_PREFIX = "[yt-chat] "
 
 -- Major credits to https://github.com/Agash/YTLiveChat for the original regexes
 
+ANIMATION_ACTIVATION_TARGET_ID='"animationActivationTargetId"%s*:%s*"([^"]*)"'
 LIVE_ID_REGEX = '<link rel="canonical" href="https://www%.youtube%.com/watch%?v=([^"]+)">'
 API_KEY_REGEX = '"INNERTUBE_API_KEY"%s*:%s*"([^"]*)"'
 CLIENT_VERSION_REGEX = '"INNERTUBE_CONTEXT_CLIENT_VERSION"%s*:%s*"([^"]*)"'
